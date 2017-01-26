12624096ba68fdebd2d0e498a001c97de554c294
Allan Wazacz <cazwazacz@gmail.com>
2017-01-26 19:37:48 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app = express()
+
+app.get('/api/whoami', function (req, res) {
+  //res.send('Hello World!')
+  var ip = req.ip
+  console.log(ip)
+  var language = req.headers['accept-language'].split(",")[0];
+  var software = req.headers['user-agent'].split("(")[1].split(")")[0]
+  console.log(language)
+  console.log(software)
+  var obj = {
+    ipaddress: ip,
+    language: language,
+    software: software
+  };
+
+  res.json(obj);
+})
+
+var port = process.argv.PORT || 3000;
+app.listen(port, function () {
+  console.log('Request-header-parser listening on port ' + port + '!')
+})
